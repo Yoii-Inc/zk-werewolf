041028d8f16eb6b4d3addbef1d10963fed4f1467
sheagrief <3a.mad1earth4@gmail.com>
2024-12-19 16:34:35 +0900

    Initial commit with 🏗️ Scaffold-ETH 2

---
+++ packages/foundry/script/Deploy.s.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "./DeployHelpers.s.sol";
+import { DeployYourContract } from "./DeployYourContract.s.sol";
+
+/**
+ * @notice Main deployment script for all contracts
+ * @dev Run this when you want to deploy multiple contracts at once
+ *
+ * Example: yarn deploy # runs this script(without`--file` flag)
+ */
+contract DeployScript is ScaffoldETHDeploy {
+    function run() external {
+        // Deploys all your contracts sequentially
+        // Add new deployments here when needed
+
+        DeployYourContract deployYourContract = new DeployYourContract();
+        deployYourContract.run();
+
+        // Deploy another contract
+        // DeployMyContract myContract = new DeployMyContract();
+        // myContract.run();
+    }
+}
---
+++ packages/foundry/test/YourContract.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../contracts/YourContract.sol";
+
+contract YourContractTest is Test {
+    YourContract public yourContract;
+
+    function setUp() public {
+        yourContract = new YourContract(vm.addr(1));
+    }
+
+    function testMessageOnDeployment() public view {
+        require(keccak256(bytes(yourContract.greeting())) == keccak256("Building Unstoppable Apps!!!"));
+    }
+}
