b26da88becc15cb5b72481e5ef02e1ebed3b4f0a
sheagrief <3a.mad1earth4@gmail.com>
2025-05-20 21:26:06 +0900

    fix: voting circuit implementation & its tests

--- packages/server/tests/zk_proof_test.rs
+++ packages/server/tests/zk_proof_test.rs
@@ -1,4 +1,12 @@
 use ark_bls12_377::Fr;
+use ark_crypto_primitives::commitment::constraints::CommitmentGadget;
+use ark_ff::BigInteger;
+use ark_ff::PrimeField;
+use ark_ff::UniformRand;
+use ark_serialize::CanonicalDeserialize;
+use ark_std::test_rng;
+use mpc_algebra::CommitmentScheme;
+use mpc_algebra::FromLocal;
 use mpc_algebra::Reveal;
 use serde_json::json;
 use wiremock::{
@@ -46,6 +54,91 @@ async fn test_request_proof() {
         "Failed to check proof status: {:?}",
         status.err()
     );
+}
+
+#[tokio::test]
+async fn test_werewolf_vote_proof() {
+    let rng = &mut test_rng();
+    let pedersen_param = <Fr as LocalOrMPC<Fr>>::PedersenComScheme::setup(rng).unwrap();
+
+    let mpc_pedersen_param = <MFr as LocalOrMPC<MFr>>::PedersenParam::from_local(&pedersen_param);
+
+    let player_randomness = (0..3).map(|_| Fr::rand(rng)).collect::<Vec<_>>();
+    let player_commitment = player_randomness
+        .iter()
+        .map(|x| {
+            <Fr as LocalOrMPC<Fr>>::PedersenComScheme::commit(
+                &pedersen_param,
+                &x.into_repr().to_bytes_le(),
+                &<Fr as LocalOrMPC<Fr>>::PedersenRandomness::default(),
+            )
+            .unwrap()
+        })
+        .collect::<Vec<_>>();
+
+    let mpc_player_commitment = player_commitment
+        .iter()
+        .map(|x| <MFr as LocalOrMPC<MFr>>::PedersenCommitment::from_public(*x))
+        .collect::<Vec<_>>();
+
+    let votes_data = vec![
+        vec![
+            MFr::from_public(Fr::from(0)),
+            MFr::from_public(Fr::from(1)),
+            MFr::from_public(Fr::from(0)),
+        ],
+        vec![
+            MFr::from_public(Fr::from(0)),
+            MFr::from_public(Fr::from(1)),
+            MFr::from_public(Fr::from(0)),
+        ],
+        vec![
+            MFr::from_public(Fr::from(0)),
+            MFr::from_public(Fr::from(0)),
+            MFr::from_public(Fr::from(1)),
+        ],
+    ];
+
+    // 投票に関する入力データ
+    let circuit = AnonymousVotingCircuit {
+        is_target_id: votes_data, // 投票対象のID
+        pedersen_param: mpc_pedersen_param,
+        player_randomness: player_randomness
+            .iter()
+            .map(|x| MFr::from_public(*x))
+            .collect::<Vec<_>>(),
+        player_commitment: mpc_player_commitment,
+    };
+
+    // 証明のリクエスト
+    let proof_id = request_proof_with_output(
+        CircuitIdentifier::Built(BuiltinCircuit::AnonymousVoting(circuit)),
+        ProofOutputType::Public,
+    )
+    .await;
+
+    // 30秒待つ
+    tokio::time::sleep(tokio::time::Duration::from_secs(30)).await;
+
+    assert!(
+        proof_id.is_ok(),
+        "証明のリクエストに失敗: {:?}",
+        proof_id.err()
+    );
+
+    // 証明の状態を確認
+    let (status, output) = check_proof_status(&proof_id.unwrap()).await.unwrap();
+    assert!(status, "証明の生成に失敗");
+    assert!(output.is_some(), "証明の出力が存在しません");
+
+    let a = output.unwrap();
+    assert!(a.value.is_some(), "証明の出力が存在しません");
+
+    let expected_output = Fr::from(1);
+    let actual_output: Fr = CanonicalDeserialize::deserialize(&mut &a.value.unwrap()[..]).unwrap();
+
+    // expected player 1 is most voted.
+    assert_eq!(actual_output, expected_output);
 }
 
 #[tokio::test]
