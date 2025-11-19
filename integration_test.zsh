b2134c0df632ebac8f0f4651bb58c98360e64cf9
sheagrief <3a.mad1earth4@gmail.com>
2025-11-19 17:17:07 +0900

    fix: Update node startup commands to include localhost addresses

--- integration_test.zsh
+++ integration_test.zsh
@@ -55,11 +55,11 @@ run_server_node_tests() {
     cargo run --release keygen --id 2
 
     # 3つのノードを起動（それぞれ異なるポートで）
-    ./target/release/zk-mpc-node start --id 0 --input ./address/3 &
+    ./target/release/zk-mpc-node start --id 0 --input ./address/3(localhost) &
     NODE0_PID=$!
-    ./target/release/zk-mpc-node start --id 1 --input ./address/3 &
+    ./target/release/zk-mpc-node start --id 1 --input ./address/3(localhost) &
     NODE1_PID=$!
-    ./target/release/zk-mpc-node start --id 2 --input ./address/3 &
+    ./target/release/zk-mpc-node start --id 2 --input ./address/3(localhost) &
     NODE2_PID=$!
 
     # ノードの起動を待機
