fb25ec11bf610d82429168e28d9172d2076f5f80
sheagrief <3a.mad1earth4@gmail.com>
2026-02-14 18:03:18 +0900

    feat: add Groth16 and Marlin Solidity verifiers
    - Added Groth16 verifier implementation in `groth16.rs` with export functionality for Solidity contracts.
    - Introduced Marlin verifier implementation in `marlin_pc.rs`, including proof structure and export functionality.
    - Created module files for Marlin and updated the main schemes module to include Groth16 and Marlin.
    - Added tests for Groth16 and Marlin verifiers to ensure correct functionality and export capabilities.
    - Implemented utility functions for formatting big integers and modulus in `utils.rs`.

---
+++ packages/arkworks-solidity-verifier/src/lib.rs
@@ -0,0 +1,37 @@
+use ark_ec::PairingEngine;
+
+pub(crate) mod constants;
+pub(crate) mod pairings;
+pub(crate) mod utils;
+
+pub mod schemes;
+
+#[cfg(test)]
+mod tests;
+
+/// Helper trait for generating library for elliptic curve group and pairing operation in Solidity.
+///
+/// This can be extended to other curve other than BN254 once precompiles are available.
+///
+/// Example:
+/// ```rust
+/// type YourCurve;
+/// impl PairingLibrary for YourCurve {
+/// // ...
+/// }
+/// ```
+pub trait PairingLibrary: PairingEngine {
+    fn template(g2_addition: bool) -> String;
+
+    fn g1_to_string(g1: &Self::G1Affine) -> String;
+
+    fn g2_to_string(g2: &Self::G2Affine) -> String;
+}
+
+/// Primary trait for proving schemes for generating Solidity verifier.
+pub trait SolidityVerifier<E: PairingLibrary> {
+    type Proof;
+    type VerifyingKey;
+
+    fn export(vk: &Self::VerifyingKey) -> String;
+}
